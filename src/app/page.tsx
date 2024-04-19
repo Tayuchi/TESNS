06c8aacbc9cdc7245327dda02753aa16cae8a2f8
dokimiki <57443812+dokimiki@users.noreply.github.com>
2024-04-19 21:50:11 +0900

    gome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import { Box } from '@mui/material';
 
 export default function Home() {
   return (
     <main>
-      this is twitter
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2} >
+          <Grid item xs={2}>test</Grid>
+          <Grid item xs={6}>test</Grid>
+          <Grid item xs={2}>test</Grid>
+        </Grid>
+      </Box>
+
     </main>
   );
 }
