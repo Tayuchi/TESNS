63484719035ee7a5c71511dbef43676f29d2f7a7
sungyo4869 <hchengjian1@gmail.com>
2024-04-20 16:48:45 +0900

    コンポーネント定義した

---
+++ src/libs/components/login/Login.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+    return (
+        <div className="flex flex-col justify-center items-center h-screen">
+            <p>Login Component</p>
+        </div>
+    )
+}
---
+++ src/libs/components/sign-up/SignUp.tsx
@@ -0,0 +1,9 @@
+import { TextField, Button, Card, CardContent, Grid } from "@mui/material"
+
+export default function SignUp() {
+    return (
+        <div className="flex flex-col justify-center items-center h-screen">
+            <p>SignUp Component</p>
+        </div>
+    )
+}
