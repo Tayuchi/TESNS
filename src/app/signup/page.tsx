683f80a33dc3e15c647b9a388c2c55dd0a08f0e1
sungyo4869 <hchengjian1@gmail.com>
2024-04-20 15:56:50 +0900

    それぞれのページで文字が出力されるようにした

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUp(){
+    return (
+        <div className="flex flex-col justify-center items-center h-screen">
+            <p>SignUp Page</p>
+        </div>
+    )
+}
