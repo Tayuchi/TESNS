797e6ecc8f6834415bc13aa48f6775d5fa761a2b
Kabuto <84449032+Kabuto0421@users.noreply.github.com>
2024-04-23 22:57:28 +0900

    side var icon (#23)

--- src/libs/components/layouts/SideBar.tsx
+++ src/libs/components/layouts/SideBar.tsx
@@ -1,14 +1,18 @@
-"use client"
-
 import React from 'react'
-import { Box, Drawer, Link, List, ListItem, ListItemButton, ListItemIcon, Toolbar } from '@mui/material'
+import { Box, Drawer, Link, List, ListItem, ListItemButton, ListItemIcon, Toolbar } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home'; // ホームアイコン
+import ExploreIcon from '@mui/icons-material/Explore'; // 探索アイコン
+import InfoIcon from '@mui/icons-material/Info'; // 情報アイコン
+import MessageIcon from '@mui/icons-material/Message'; // メッセージアイコン
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'; // プロフィールアイコン
+import AddBoxIcon from '@mui/icons-material/AddBox'; // 投稿するアイコン
 
 const SideBar = () => {
   return (
     <div>
       <Drawer
         variant='permanent'
-        sx={{ width: 240, flexShrink: 0, }}
+        sx={{ width: 240, flexShrink: 0 }}
       >
         <Toolbar />
         <Box sx={{ overflow: 'auto' }}>
@@ -16,18 +20,48 @@ const SideBar = () => {
             <ListItem>
               <ListItemButton>
                 <ListItemIcon>
-                  <Link href='/'>
-                    Home
-                  </Link>
+                  <HomeIcon />
+                  <Link href='/' color="inherit">Home</Link>
                 </ListItemIcon>
               </ListItemButton>
             </ListItem>
             <ListItem>
               <ListItemButton>
                 <ListItemIcon>
-                  <Link href='/'>
-                    About
-                  </Link>
+                  <ExploreIcon />
+                  <Link href='/' color="inherit">Explore</Link>
+                </ListItemIcon>
+              </ListItemButton>
+            </ListItem>
+            <ListItem>
+              <ListItemButton>
+                <ListItemIcon>
+                  <InfoIcon />
+                  <Link href='/' color="inherit">About</Link>
+                </ListItemIcon>
+              </ListItemButton>
+            </ListItem>
+            <ListItem>
+              <ListItemButton>
+                <ListItemIcon>
+                  <MessageIcon />
+                  <Link href='/' color="inherit">Message</Link>
+                </ListItemIcon>
+              </ListItemButton>
+            </ListItem>
+            <ListItem>
+              <ListItemButton>
+                <ListItemIcon>
+                  <AccountCircleIcon />
+                  <Link href='/' color="inherit">Profile</Link>
+                </ListItemIcon>
+              </ListItemButton>
+            </ListItem>
+            <ListItem>
+              <ListItemButton>
+                <ListItemIcon>
+                  <AddBoxIcon />
+                  <Link href='/' color="inherit">Post</Link>
                 </ListItemIcon>
               </ListItemButton>
             </ListItem>
