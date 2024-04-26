b9248eb256d059c3f520d206f28de3a59815f171
sungyo4869 <hchengjian1@gmail.com>
2024-04-26 10:54:08 +0900

    プロフィール画面から戻れるようにした

--- src/libs/components/profile/Header.tsx
+++ src/libs/components/profile/Header.tsx
@@ -5,16 +5,18 @@ import { ArrowBack } from "@mui/icons-material";
 
 export default function Header() {
     return (
-        <Box sx={{ position: 'fixed', backgroundColor: '#FFFFFF', padding: 1 , width: '100%', zIndex: 9999}}>
+        <Box sx={{ position: 'fixed', backgroundColor: '#FFFFFF', padding: 1, width: '100%', zIndex: 9999 }}>
             <Grid container alignItems="center">
-                <Grid item xs={1}>
-                    <ArrowBack />
+                <Grid item xs={1.5}>
+                    <Button href="/" style={{ padding: 0 }}>
+                        <ArrowBack />
+                    </Button>
                 </Grid>
-                <Grid item xs={11}>
+                <Grid item xs={10.5}>
                     <Typography fontWeight="fontWeightBold" color={'#0f1419'} sx={{ fontSize: '20px' }}>
                         すんぎょ
                     </Typography>
-                    <Typography  sx={{ fontSize: '13px' }} color={'#0f1419'}>
+                    <Typography sx={{ fontSize: '13px' }} color={'#0f1419'}>
                         ポスト数
                     </Typography>
                 </Grid>
