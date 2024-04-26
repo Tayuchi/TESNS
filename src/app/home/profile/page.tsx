21bd73c893055ebdacf69a42d1d7a88ef50eeef7
sungyo4869 <hchengjian1@gmail.com>
2024-04-26 11:25:17 +0900

    Profile画面の見た目できた

---
+++ src/app/home/profile/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import Profile from "@/libs/components/profile/Profile"
+
+export default function ProfilePage() {
+    return (
+        <div >
+            <Profile />
+        </div>
+    )
+}
