ae6def6cdc7989a0ce890f56eb43df2a0d844d79
sungyo4869 <hchengjian1@gmail.com>
2024-04-27 13:44:27 +0900

    画面半分になっても表示されるようになった

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,16 @@
-import Image from "next/image"
+import Image from "next/image";
 
 export default function NotFound() {
     return (
         <div className="flex flex-col justify-center items-center h-screen">
-            <h1>ぬぬぬ</h1>
             <Image
                 src="/GMB5sxtbYAAOUJJ.png"
-                width={1080}
-                height={360}
+                width={450}
+                height={253}
                 layout="fixed"
                 objectFit="cover"
-                className="hidden md:block"
                 alt=""
             />
         </div>
-    )
+    );
 }
