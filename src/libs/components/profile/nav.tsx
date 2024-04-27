4e080babdff608365a80f7f6851a7613fbdb2692
Kabuto <84449032+Kabuto0421@users.noreply.github.com>
2024-04-27 16:31:34 +0900

    Issue 2 自分のアカウント画面のUI (#55)
    * プロフィールにfirestoreの情報持ってきたー
    
    * account screen完成
    
    * account screen準備完了

--- src/libs/components/profile/nav.tsx
+++ src/libs/components/profile/nav.tsx
@@ -4,19 +4,17 @@ import { Box, ListItem, Button } from '@mui/material';
 const sidebarItems = [
   { text: 'Posts', href: '/' },
   { text: 'Replies', href: '/' },
-  { text: 'Highlights', href: '/' },
-  { text: 'Articles', href: '/' },
   { text: 'Media', href: '/' },
   { text: 'Likes', href: '/' },
 ];
 
 export default function Nav() {
   return (
-    <Box sx={{ display: 'flex', width: 1, margin:0 }}>
+    <Box sx={{ display: 'flex', width: 1, margin: 0 }}>
       {sidebarItems.map((item, index) => (
         <ListItem key={index} sx={{ padding: 0 }}>
           <Button sx={{ padding: 1 }} href={item.href} color='inherit'>
-              {item.text}
+            {item.text}
           </Button>
         </ListItem>
       ))}
